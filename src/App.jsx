69339dd9776646fad557f3795c38818c38bd8074
Fatima Ait Brik <aitbrikfatima75@gmail.com>
2026-05-13 19:39:17 +0100

    Initial setup with React + Vite + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-slate-950 min-h-screen text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-violet-500">
+        DevApply
+      </h1>
+    </div>
+  )
+}
+
+export default App
